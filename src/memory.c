a4e80bb1051a4ff8966d5e795af618796ee249b3
tomoyasuzuki <tomoya.s0121@icloud.com>
2020-08-05 23:34:01 +0900

    fix: restore init_memory()

--- src/memory.c
+++ src/memory.c
@@ -2,10 +2,10 @@
 #include "window.h"
 
 void init_memory(MemoryMap *memmap) {
-    uint64_t *base = (uint64_t*)memmap->buff;
+    uint64_t base = (uint64_t)memmap->buff;
 
-    for (uint64_t i = 0; i < (uint64_t)base + memmap->size; i += memmap->desc_size) {
-        MemoryDescriptor *mem_desc = (MemoryDescriptor*)base[i];
+    for (uint64_t i = base; i < base + memmap->size; i += memmap->desc_size) {
+        MemoryDescriptor *mem_desc = (MemoryDescriptor*)i;
         
         if (mem_desc->type == kEfiConventionalMemory) {
             char *s = "Find EfiConventionalMemory";
