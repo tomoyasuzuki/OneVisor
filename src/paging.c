4258a6394fe8e8490c3f01c5b74d448d876ef9d8
tomoyasuzuki <tomoya.s0121@icloud.com>
2020-08-15 14:57:08 +0900

    feature: add paging initialization function

---
+++ src/paging.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include "paging.h"
+#include "assembly.h"
+#include <stdalign.h>
+
+#define PageSize4K 4096
+#define PageSize2M 512 * PageSize4K
+#define PageSize1G 512 * PageSize2M
+
+alignas(PageSize4K) uint64_t pml4_table[512];
+alignas(PageSize4K) uint64_t pdp_table[512];
+alignas(PageSize4K) uint64_t page_directory[64][512];
+
+void init_paging() {
+    pml4_table[0] = (uint64_t)(&pdp_table[0]) | 0x003;
+    for (int i_pdpt = 0; i_pdpt < (int)sizeof(page_directory); ++i_pdpt) {
+        pdp_table[i_pdpt] = (uint64_t)(&page_directory[i_pdpt]) | 0x003;
+        for (int i_pd = 0; i_pd < 512; ++i_pd) {
+            page_directory[i_pdpt][i_pd] = i_pdpt * PageSize1G + i_pd * PageSize2M | 0x083;
+        }
+    }
+
+    write_cr3((uint64_t)&pml4_table[0]);
+    uint64_t current_cr0 = read_cr0();
+    write_cr0(current_cr0 & 0xfffeffff);
+}
+
+
--- src/vmm_entry.c
+++ src/vmm_entry.c
@@ -2,6 +2,7 @@
 #include "window.h"
 #include "memory.h"
 #include "assembly.h"
+#include "paging.h"
 #include <stdint.h>
 
 void vmmEntry(BootInfo *boot_info) {
@@ -9,17 +10,8 @@ void vmmEntry(BootInfo *boot_info) {
     init_graphic(boot_info->frame_buff);
     blackout_window();
     
-    char *success_cr3 = "Get CR3";
-    char *success_cr0 = "Get CR0";
-    
-    uint64_t cr3 = read_cr3();
-    uint64_t cr0 = read_cr0();
+    char *entry_message = "OneVisor";
+    put_s(entry_message);
 
-    if (cr3 != 0) {
-        put_s(success_cr3);
-    }
-
-    if (cr0 != 0) {
-        put_s(success_cr0);
-    }
+    init_paging();
 }
